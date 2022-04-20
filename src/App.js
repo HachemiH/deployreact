854832fa3135d2b7224e08cb0cc72a5d48a5d051
Hachemi <HachemiH@users.noreply.github.com>
2022-04-20 12:43:45 +0200

    edit homepage

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
-          Bonjour le Monde
+          Bonjour le Quartier
         </a>
       </header>
     </div>
